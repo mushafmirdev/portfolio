7e78963969cecab5462c18958a1de7c57b92ad69
Mushaf Ali <168913974+mushafmirdev@users.noreply.github.com>
2024-09-10 22:48:33 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelectorAll('nav ul li a').forEach(link => {
+    link.addEventListener('click', (e) => {
+        e.preventDefault();
+        const targetId = link.getAttribute('href').substring(1);
+        document.getElementById(targetId).scrollIntoView({ behavior: 'smooth' });
+    });
+});
